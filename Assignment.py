3d8289dbb15b0449e4c66deaaa4a678644c82d74
anusoorya <89992970+anusoorya@users.noreply.github.com>
2021-09-20 00:19:38 +0530

    Assignment.py
    Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,23 @@
+#Listmethods
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("Grapes")
+print(fruits)
+fruits = ['apple', 'banana', 'cherry']
+fruits.clear()
+print(fruits)
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.copy()
+print(fruits)
+fruits = ['apple', 'banana', 'cherry']
+fruits.reverse()
+print(fruits)
+
+#strings 
+
+text= "hi , how are you "
+x = text.capitalize()
+print (x)
+text= "hi , how are you "
+x = text.count("you")
+print(x)
